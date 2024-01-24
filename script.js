592584067bb526133fd8be67d9c5f766ab2d81d3
ananoesatia <133655590+ananoesatia@users.noreply.github.com>
2024-01-24 16:58:20 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -46,5 +46,16 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    document.querySelector(".menu").addEventListener("click", ()=>{
+        document.querySelector(".nav-bar").classList.add("active")
+    })
+    document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".nav-bar").classList.remove("active")
+    })
+    document.querySelectorAll(".element").forEach((element)=>{
+        element.addEventListener("click", ()=>{
+            document.querySelector(".nav-bar").classList.remove("active")
+        })
+    })
 });
-
+ 
